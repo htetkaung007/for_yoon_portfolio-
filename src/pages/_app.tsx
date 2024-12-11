01c7a90353781513795455d2ce19231f46c1e425
Htet Linn <htetkaunglinn12345@gamil.com>
2024-12-11 17:01:50 +0630

    Same solve error 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,18 @@ import ThemeProviderWrapper from "@/components/utils/ThemeProviderWrapper";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
 
+const cache = createCache({ key: "css", prepend: true });
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProviderWrapper>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProviderWrapper>
+    <CacheProvider value={cache}>
+      <ThemeProviderWrapper>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProviderWrapper>
+    </CacheProvider>
   );
 }
